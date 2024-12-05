54726310cb09c6eff8d9781825af2a87a8cca8e9
Anzhc <133806049+Anzhc@users.noreply.github.com>
2024-12-05 23:21:25 +0200

    Add check for .41 and .42. Handling for post-training saves.
    Check for compromised versions. Handling for saving without dill below and above 8.3.0 post training.
    Comment about plots=False while using wandb on Ultralytics below 8.3.20.

--- Training.py
+++ Training.py
@@ -1,21 +1,31 @@
+import ultralytics
 from ultralytics import YOLO # checks, hub
 import os
+from packaging import version
+import sys
 # checks() # Needed for training with Ultralytics hub
 # hub.login('YOUR API KEY')
 
-model_name = 'Model Name'
-project_name = 'Project'
+ultralytics_version = ultralytics.__version__
+if version.parse(ultralytics_version) == version.parse("8.3.41") or version.parse(ultralytics_version) == version.parse("8.3.42"):
+    print("WARNING: Versions 8.3.41 and 8.3.42 are compromised with cryto miner. REINSTALL ULTRALYTICS.")
+    print("TERMINATING")
+    sys.exit()
+
+model_name = 'YOUR MODEL NAME'
+project_name = 'YOUR PROJECT NAME'
+
 
 def main():
-    model = YOLO('yolov8n-seg.pt') #name of pretrained model.
+    model = YOLO('yolov8n.pt') #name of pretrained model.
+    #P.S. v11 doesn't have "v" before number, i.e. - `yolo11n`
     #It will be downloaded, or you can assemble it from scratch, by using .yaml instead of .pt.
     #like this: model = YOLO('yolov8n.yaml')
     #I think not all models are available in pretrained format, so this might be necessary sometimes.
 
-    # They always start with 'yolovX' where X stands for version of yolo model. Right now 8 is most modern.
-    # Sizes for v8: n, s, m, l, x; - from smallest to biggest. i.e. yolov8n for smallest.
-    # Sizes for v9(DETECTION ONLY): t, s, m, c, e; SEG: c, e.
-    # Sizes for v10(DETECTION ONLY): n, s, m, l, x.
+    # They always start with 'yolovX' where X stands for version of yolo model. For yolo11 "n" is ommited: "yolo11X"
+    # Right now 11 is most modern. v8 and v11 should be supporting same sizes and modes, possibly direct upgrade path.
+    # Sizes: n, s, m, l, x; - from smallest to biggest. i.e. yolov8n for smallest. Also c and e for v9.
     # Then there are various families of models for various tasks.
     # No classifier - general model for detection.
     # `-seg`- segmentation model.
@@ -30,85 +40,82 @@ def main():
 
     results = model.train(
         #[Data and Project setup]
-        data='Path\\To\\Your\\Data.yaml',    #Path to your .yaml for dataset
+        data='YOUR\\DATA\\PATH',    #Path to your .yaml for dataset
                                                     # or to general folder with train and val subfolders, if classification.
         name=model_name,                     #Model name
-        project=project_name,              # Folder name, if needed.
-        exist_ok=True,            # Allow  overwrite or not, if already exist.
-        #format=torchscript         # Default format. Don't touch. Or do if you need ONNX or something.
-              
+        project=project_name,          # Project name, if needed. Will separate metric logging into specific project.
+        exist_ok=True,            # Allow  overwrite or not, of already exist.  
+        #IMPORTANT: with both 'project' and 'exist_ok' set, don't forget to change name, or wipe folder of project,
+        #Otherwise Pandas will error out.
         ###       [Training Setup]###
         pretrained=True,            # Train from pretrained model or not.
-        #imgsz=1024,                # Image size to train on
-        epochs=500,                 # Number of epochs
+        imgsz=640,                # Image size to train on
+        epochs=50,                 # Number of epochs
         #time=None,                 # Max time to train for
-        patience=120,               # Amount of epochs to train for before stopping, if no improvement is observed
+        patience=25,               # Amount of epochs to train for before stopping, if no improvement is observed
         cache="ram",                # Caching. None/disk/ram
-        batch=-1,                 # Batch default is 16. Can be used with -1, or with float 0 - 1. i.e. 0.55, this will use batch corresponding to ~55% VRAM usage.
-        device="0",                   # Which GPU to use. Can specify multiple, but they are unlikely to work.
+        batch=0.8,                 # Batch default is 16. -1 for auto
+        device="0",                   # Which GPU to use
         #rect=True,                 # Optimize batches for minimal padding. Can hurt accuracy.
         save=True,                  # Save model
         #save_period=-1,            # How often to save. By default saves only best and last.
-        workers=4,                 # For loading. I guess that's CPU threads per GPU.
+        workers=2,                 # For loading. I guess that's CPU threads per GPU.
 
         seed=420420,                # Seed for determinism
-        #resume=False,              # resume training, if resuming. P.S. Idk how this one works tbh.
+        #resume=False,              # resume training, if resuming
 
         #   [Dataset Augmentation]###
-        hsv_h=0.35,                  # Hue randomization in training, if needed. Don't use for color-critical models.
-        hsv_s=0.25,                  # Saturation randomization. Don't use for color-critical models.
-        hsv_v=0.15,                  # Value randomization. Don't use for color-critical models.
+        hsv_h=0.15,                  # Hue randomization in training, if needed. Don't use for color-critical models. #0.35
+        hsv_s=0.15,                  # Saturation randomization. Don't use for color-critical models. #0.25
+        hsv_v=0.15,                  # Value randomization. Don't use for color-critical models. #0.15
         #bgr = 0.5,                  # Change rgb channels to bgr.
-        degrees=45,                 # Max random rotation degree. Don't use for orientation-critical model.
-        flipud=0.25,                # Flip upside-down chance. Don't use for orientation-critical models.
-        fliplr=0.5,                 # Flip left-right chance. Don't use for orientiation-critical models.
-        #translate=0.1,              # Translation of image during training(?).
-        scale=0.5,                  # Random scaling of images.
-        #shear=0.0,                  # Shear(?)
-        #perspective=0.0005,            # Ultralytics suggested range 0 to 0.001.
-        #mixup=0.0,                  # Mixup(?)
+        degrees=15,                 # Max random rotation degree. Don't use for orientation-critical model. #45
+        flipud=0.25,                # Flip upside-down chance. Don't use for orientation-critical models.#0.5
+        fliplr=0.25,                 # Flip left-right chance. Don't use for orientiation-critical models.#0.5
+        #translate=0.1,              # Translation of image during training(?).#0.1
+        scale=0.05,                  # Random scaling of images. # 0.5
+        #shear=0.0005,                  # Shear(?)
+        #perspective=0.00005,            # Ultralytics suggested range 0 to 0.001.# 0.0005
+        #mixup=0.0,                  # Mixup. Mixes different images together to create a sort of "combo"
         #copy_paste=0.0,             # Probability of copy-paste of segment instances.
         #auto_augment="randaugment", # Automatic mode for augments. Values: "randaugment", "autoaugment", "augmix"
         #erasing=0.0,                # Probability of random erasing during CLASSIFICATION training.
-        mosaic=1,                   # Mosaic. Rearranges images and data in batch to strongly augment it.
-        
+        mosaic=1,                   # Mosaic. Rearranges images and data to strongly augment it.
         close_mosaic=0,            # Stop mosaic augment for last N epochs.
+        
 
         # [Other Dataset Settings]###
         #fraction=1.0,              # Fraction of dataset to train on.
 
 
         #  [Optimizing Parameters]###
-        optimizer='auto',           # Optimizer. Auto sets everything. SGD, Adam, AdamW, NAdam, RAdam, RMSProp, etc. Auto is good.
-        #lr0=0.01,                  # Starting LR. Overwritten by Auto.
-        #lrf=0.01,                  # Final LR. Starting LR multiplied by lrf. i.e. in this case 0.01 * 0.01 = 0.0001. Overwritten by Auto.
-        #momentum=0.937,            # Momentum for optimizers that use it. For others this will set beta1. Overwritten by Auto.
-        #weight_decay=0.0005,       # L2 regularization. Overwritten by Auto.
-        #warmup_epochs=5.0,         # Warmup epochs amount.
+        optimizer='auto',           # Optimizer. Auto sets everything. SGD, Adam, AdamW, NAdam, RAdam, RMSProp, etc.
+        #lr0=0.00005,                  # Starting LR.
+        #lrf=0.01,                  # Final LR. Starting LR multiplied by lrf. i.e. in this case 0.01 * 0.01 = 0.0001
+        #momentum=0.99,            # Momentum for optimizers that use it. For others this will set beta1.
+        #weight_decay=0.002,       # L2 regularization.
+        warmup_epochs=5,         # Warmup epochs amount.
         #warmup_momentum=0.8,       # Warmup for momentum. I guess it will start either at flat 0.8, or at 80% of final momentum.
-        #warmup_bias_lr=0.1,        # Starting fraction of LR for bias parameters. Helps to stabilize model in initial epochs?
-        overlap_mask=False,         # Allow or disallow mask overlap in training.
+        #warmup_bias_lr=0.0,        # Starting fraction of LR for bias parameters. Helps to stabilize model in initial epochs?
+        overlap_mask=True,         # Allow or disallow mask overlap in training.
         cos_lr=True,                # Cosine or linear LR.
-        single_cls=False,           # Single class training.If you want your multi-class dataqsets to be trated as single.
-        mask_ratio=1,               # Downsample of segmentation masks. Default is 4.
+        #single_cls=False,           # Single class training.
+        mask_ratio=1,               # Ratio by which dataset mask inputs will be divided by. i.e. it will remove half of points. Default is 4.
         deterministic=True,         # Determinism.
-        #dropout=0.1,               # Dropout.
+        dropout=0.1,               # Dropout.
 
         #[Advanced Training Params]##
         freeze=None,                # Freeze first N layers, or specific layers by index.
         #box=7.5,                   # Weight of box loss. Influences emphasis of accurate prediction of boxes.
-        #cls=0.5,                   # Classification loss. Influences improtance of correct class prediction.
+        #cls=2,                   # Classification loss. Influences improtance of correct class prediction.
         #dfl=1.5,                   # Distribution focal loss. Not sure if useful, as it state that it's used in certain YOLOs.
         #pose=12.0,                 # Pose loss weight. Useful only for pose training. Influences importance of keypoints.
         #kobj=2.0,                  # Keypoint objectness(?) loss in pose estimation. alancing confidence with accuracy.
-        #label_smoothing=0.0,       # Softening hard labels to a mix of a target label and a uniform distribution over labels. Can improve generalization. - from Ultralytics wiki.
         #nbs=64,                    # Nominal batch size for normalization of loss. I guess this is like Gradient accumulation.
-
-
-        #retina_masks=Flase,        # This is not in ultralytics docs in training params. If works, will heavily affect results
+        #retina_masks=True,        # This is not in ultralytics docs in training params, but is passed in training. If works, will heavily affect results
                                         # as it will draw masks at resolution of image, instead of 160x160.
-        #multi_scale=False,          #Training in variable size. i.e. 640 training could range from some ~250px to ~850px or so.
-        #WARNING: Unstable VRAM usage. High possibility of OOM, if close to limit.
+        #multi_scale=False,          # Multires training.
+
         #          [Optimizations]###
         amp=True,                   # Automatic Mixed Precision. 
 
@@ -119,29 +126,30 @@ def main():
 
         #                  [Other]###
         val=True,                   # To val, or not to val.
-        #plots=False,               # Generate and save plots of training and val metrics and prediction examples.
+        plots=True,               # Generate and save plots of training and val metrics and prediction examples.
+        #Can't use False plots w/ wandb logging if version is below 8.3.20, where i fixed it.
 
         ###[Found, but undocumented params]###
         # Here are more params that are part of parsed command(with default values), but are not explicitly mentioned
-        # in training parameters in Ultralytics docs. Some are removed, as i know they are part of predict or other modes.
+        # in training parameters in Ultralytics docs.
 
         #cfg=None                   # No idea.
         #workspace=4                # No idea.
         #opset=None                 # No idea.
-        #simplify=False             # No idea.
+        #simplify=Flase             # No idea.
         #dynamic=False              # No idea.
         #int8=False                 # int8 precision?
         #optimize=False             # No idea.
         #keras=False                # Keras format?
+        #format=torchscript         # Default format. Don't touch. Or do if you need ONNX or something.
         #agnostic_nms=False         # No idea.
         #dnn=False                  # No idea.
         #save_hybrid=False          # No idea.
         #save_json=False            # No idea.
+        #split=val                  # No idea.
 
     )
 
-    #This section is for additional saving of model WITHOUT `dill` serialization.
-    #Also renames it according to set name.
     base_path = os.path.dirname(os.path.abspath(__file__))
     best_path = os.path.join(base_path, project_name, model_name, 'weights', 'best.pt')
     last_path = os.path.join(base_path, project_name, model_name, 'weights', 'last.pt')
@@ -149,8 +157,16 @@ def main():
     last_save_path = os.path.join(base_path, project_name, model_name, 'weights', f'{model_name}_no_dill_last.pt')
     best_model = YOLO(best_path)
     last_model = YOLO(last_path)
-    best_model.save(filename=best_save_path, use_dill=False)
-    last_model.save(filename=last_save_path, use_dill=False)
+
+    ultralytics_version = ultralytics.__version__
+    print(ultralytics_version)
+    if version.parse(ultralytics_version) >= version.parse("8.3.0"):
+        best_model.save(filename=best_save_path)
+        last_model.save(filename=last_save_path)
+    else:
+        # Old version, use use_dill=False
+        best_model.save(filename=best_save_path, use_dill=False)
+        last_model.save(filename=last_save_path, use_dill=False)
 
 
 if __name__ == '__main__':
